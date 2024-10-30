783312a2dffc3310501ab530e87fff68b2630a9f
Dail0112 <158965066+Dail0112@users.noreply.github.com>
2024-10-30 08:44:35 -0600

    Add files via upload

---
+++ FormProducto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntradaEjemplo
+{
+   public partial class Form1 : 
+      MetroFramework.Forms.MetroForm
+
+   {
+      public Form1()
+      {
+         InitializeComponent();
+         this.StyleManager = metroStyleManager1;
+      }
+
+      private void Form1_Load(object sender, EventArgs e)
+      {
+
+      }
+
+      private void metroTextBox1_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void metroToggle1_CheckedChanged(object sender, EventArgs e)
+      {
+         if(metroToggle1.Checked)
+         {
+            metroStyleManager1.Theme =
+               MetroFramework.MetroThemeStyle.Dark;
+         }
+         else
+         {
+            metroStyleManager1.Theme =
+               MetroFramework.MetroThemeStyle.Light;
+         }
+      }
+   }
+}
